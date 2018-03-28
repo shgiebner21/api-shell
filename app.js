854ace4412ecd057cd2bd0d1d15432e1ac7619d8
Stephen Giebner <shgiebner21@gmail.com>
2018-03-28 09:04:27 -0400

    initial commit

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const bodyParser = require('body-parser')
 const HTTPError = require('node-http-error')
 const cors = require('cors')
 
-const {postFamily, postChildren, listChildren,
-       getChild, listBadges, getFamilies, getFamily, getParks,
-       getPark, getActivity, updateChild} = require('./dal.js')
+const {  } = require('./dal.js')
 
 app.use(cors({
     credentials: true
@@ -15,114 +13,12 @@ app.use(cors({
 app.use(bodyParser.json())
 
 
-console.log('parker-and-parks-api has received a call.')
 
 /////////////////////////////////////////////
-//   family
+//   function section
 /////////////////////////////////////////////
 
-app.post('/family', function(req, resp, next) {
-  console.log('app.post req.body structure is ', req.body)
-  postFamily(req.body, function(err, dalResp) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(201).send(dalResp)
-  })
-})
 
-app.get('/family', function(req, resp, next) {
-  const startKey = req.query.startkey ? req.query.startkey : undefined
-  const limit = req.query.limit ? req.query.limit : undefined
-
-  getFamilies(function(err, dalResp) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(dalResp)
-  })
-})
-
-
-app.get('/family/:id', function(req, resp, next) {
-  getFamily(req.params.id, function(err, family) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(family)
-  })
-})
-
-
-
-/////////////////////////////////////////////
-//   children
-/////////////////////////////////////////////
-app.post('/children', function(req, resp, next) {
-  postChildren(req.body, function(err, dalResp) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(201).send(dalResp)
-  })
-})
-
-app.get('/children', function(req, resp, next) {
-  const startKey = req.query.startkey ? req.query.startkey : undefined
-  const limit = req.query.limit ? req.query.limit : undefined
-
-  listChildren(function(err, children) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    return resp.status(200).send(children)
-  })
-})
-
-
-app.get('/children/:id', function(req, resp, next) {
-  getChild(req.params.id, function(err, child) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(child)
-  })
-})
-
-app.get('/parks/:id/activitydetail/:id', function(req, resp, next) {
-  getActivity(req.params.id, function(err, activity) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(activity)
-  })
-})
-
-app.put('/children/:id', function(req, resp, next) {
-  updateChild(req.body, function(err, child) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(child)
-  })
-})
-
-
-
-
-/////////////////////////////////////////////
-//   badges
-/////////////////////////////////////////////
-app.get('/badges', function(req, resp, next) {
-
-  listBadges(function(err, badges) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(badges)
-  })
-})
-
-/////////////////////////////////////////////
-//   park
-/////////////////////////////////////////////
-
-app.get('/parks', function(req, resp, next) {
-
-  getParks(function(err, parks) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(parks)
-  })
-})
-
-app.get('/parks/:id', function(req, resp, next) {
-  getPark(req.params.id, function(err, park) {
-    if (err) return next(new HTTPError(err.status, err.message, err))
-    resp.status(200).send(park)
-  })
-})()
 
 
 
@@ -136,5 +32,5 @@ app.use(function(err, req, resp,next) {
 })
 
 app.listen(port, function() {
-  console.log('Parker API is up and running on port ', port)
+  console.log('\x1b[32m' ,'api listening on port ' + port, '\x1b[0m')
 })
--- dal.js
+++ dal.js
@@ -17,129 +17,20 @@ const {map} = require('ramda')
 
 
 /////////////////////////////////////////////
-//   family
+//   function section
 /////////////////////////////////////////////
-function postFamily(family, cb) {
-  console.log('inside dal.js postFamily()')
-  family.type = 'family'
-  let newId = 'family_' + family.parentLast.toLowerCase()
-  + '_' + family.eMail.toLowerCase() + '_' + family.cellPhone
-  family._id = newId
 
-  db.insert(family, function(err, resp) {
-    if (err) return cb(err)
-    cb(null, resp)
-  })
-}
-
-function getFamilies(cb) {
-  db.view('families', 'families', {q: 'type: family', include_docs: true}, function(err, families) {
-    if (err) return cb(err)
-    cb(null, map(returnDoc, families.rows))
-  })
-}
-
-
-function getFamily(id, cb) {
-  db.get(id, function(err, family) {
-    if (err) return cb(err)
-    cb(null, family)
-  })
-}
-
-/////////////////////////////////////////////
-//   children
-/////////////////////////////////////////////
-function postChildren(child, cb) {
-  child.type = 'children'
-  let newId = 'children_' + child.childName.toLowerCase()
-  + '_' + child.familyId.toLowerCase()
-  child._id = newId
-
-  db.insert(child, function(err, resp) {
-    if (err) return cb(err)
-    cb(null, resp)
-  })
-}
-
-function listChildren(cb) {
-  db.view('children', 'children', {q: 'type: children', include_docs: true}, function(err, children) {
-    if (err) return cb(err)
-    cb(null, map(returnDoc, children.rows))
-  })
-}
-
-
-function getChild(id, cb) {
-  db.get(id, function(err, child) {
-    if (err) return cb(err)
-    cb(null, child)
-  })
-}
-
-function updateChild(child, cb) {
-  db.insert(child, function(err, resp) {
-    if (err) return cb(err)
-    cb(null, resp)
-  })
-}
-
-
-function getActivity(id, cb) {
-  db.get(id, function(err, activity) {
-    if (err) return cb(err)
-    cb(null, activity)
-  })
-}
-
-
-/////////////////////////////////////////////
-//   badges
-/////////////////////////////////////////////
-function listBadges(cb) {
-  db.view('badges', 'badges', {q: 'type: badge', include_docs: true}, function(err, list) {
-    if (err) return cb(err)
-    cb(null, map(returnDoc, list.rows))
-  })
-}
-
-/////////////////////////////////////////////
-//   park
-/////////////////////////////////////////////
-function getParks(cb) {
-  db.view('parks', 'parks', {q: 'type: park', include_docs: true},  function(err, parks) {
-      if (err) return cb(err)
-      cb(null, map(returnDoc, parks.rows))
-    })
-}
-
-function getPark(id, cb) {
-  db.get(id, function(err, park) {
-    if (err) return cb(err)
-    cb(null, park)
-  })
-}
 
 
 /////////////////////////////////////////////
 // helper functions
 /////////////////////////////////////////////
 
-const returnDoc = row => row.doc
+
 
 
 const dal = {
-  postFamily: postFamily,
-  getFamilies: getFamilies,
-  getFamily: getFamily,
-  postChildren: postChildren,
-  updateChild: updateChild,
-  listChildren: listChildren,
-  getChild: getChild,
-  listBadges: listBadges,
-  getParks: getParks,
-  getPark: getPark,
-  getActivity: getActivity
+
 }
 
 module.exports = dal
